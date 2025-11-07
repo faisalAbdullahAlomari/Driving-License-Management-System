f1482fc390bde85307fd5fe1f7d35c74384d1e1d
Faisal Alomari <faisalakalomari@gmail.com>
2025-11-08 00:04:51 +0300

    Added the 2 new pages in the last commit

---
+++ PresentationLayer/Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentationLayer
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        People pplForm = new People();
+
+        private void btnPeople_Click(object sender, EventArgs e)
+        {
+            pplForm.ShowDialog();
+        }
+
+    }
+}
---
+++ PresentationLayer/People.cs
@@ -0,0 +1,67 @@
+﻿using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PresentationLayer
+{
+    public partial class People : Form
+    {
+        public People()
+        {
+            InitializeComponent();
+        }
+
+        private void _RefreshPeopleLV()
+        {
+
+            DataTable dtPeople = BusinessLayer.clsPeople.Find();
+
+            lvPeople.Items.Clear();
+            lvPeople.Columns.Clear();
+
+            lvPeople.View = View.Details;
+            lvPeople.FullRowSelect = true;
+            lvPeople.GridLines = true;
+
+            foreach (DataColumn column in dtPeople.Columns)
+            {
+                lvPeople.Columns.Add(column.Caption);
+            }
+
+            foreach (DataRow row in dtPeople.Rows)
+            {
+                ListViewItem item = new ListViewItem(row[0].ToString());
+
+                for (int i = 1; i < dtPeople.Columns.Count; i++)
+                {
+                    item.SubItems.Add(row[i].ToString());
+                }
+
+                lvPeople.Items.Add(item);
+            }
+
+            lvPeople.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+
+            int CountListRows = lvPeople.Items.Count;
+
+            lblCount.Text = CountListRows.ToString();
+        }
+
+        private void People_Load(object sender, EventArgs e)
+        {
+            _RefreshPeopleLV();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
