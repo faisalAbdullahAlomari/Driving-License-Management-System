2fadea74690310a3e2cd7dfb8983a3e1666ab80b
Faisal Alomari <faisalakalomari@gmail.com>
2025-11-10 22:14:44 +0300

    Completed the AddNewPerson method in the DataAccessLayer/clsDataAccessLayer class and the call in the BusinessLayer/clsPerson class

--- BusinessLayer/clsPerson.cs
+++ BusinessLayer/clsPerson.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using DataAccessLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,7 +11,19 @@ namespace BusinessLayer
     public class clsPerson
     {
 
+        private bool _AddNewPerson()
+        {
+            this.PersonID = clsDataAccess.AddNewPerson(this.NationalNumber, this.FirstName, this.SecondName, this.ThirdName, this.LastName, this.DateOfBirth, this.Gender, this.Address, this.Phone, this.Email, this.NationalityCountryID, this.ImagePath);
+        
+            return (this.PersonID != -1);
+        }
+
+        enum enMode { AddNewPerson = 1, UpdatePerson = 2 }
+
+        enMode Mode = enMode.AddNewPerson;
+
         int PersonID { get; set; }
+        string NationalNumber { get; set }
         string FirstName { get; set; }
         string SecondName { get; set; }
         string ThirdName { get; set; }
@@ -36,6 +50,27 @@ namespace BusinessLayer
             this.Email = "";
             this.NationalityCountryID = -1;
             this.ImagePath = "";
+        }
+
+        public bool Save()
+        {
+
+            switch (Mode)
+            {
+                case enMode.AddNewPerson:
+                    if (_AddNewPerson())
+                    {
+                        Mode = enMode.UpdatePerson;
+                        return true;
+                    }
+                    else
+                    {
+
+                        return false;
+                    }
+                default:
+                    return false;
+            }
         }
     }
 }
--- DataAccessLayer/clsDataAccess.cs
+++ DataAccessLayer/clsDataAccess.cs
@@ -4,7 +4,7 @@ using System.Data.SqlClient;
 
 namespace DataAccessLayer
 {
-    public class clsDataAccess
+    public static class clsDataAccess
     {
 
         public static bool FindUserByUserNameAndPassword(ref int UserID, ref int PersonID, string UserName, string Password, ref bool IsActive)
@@ -81,6 +81,79 @@ namespace DataAccessLayer
             }
 
             return dtPeople;
+        }
+
+        public static int AddNewPerson(string NationalNumber, string FirstName, string SecondName, string ThirdName, string LastName, DateTime DateOfBirth, bool Gender, string Address, string Phone, string Email, int NationalityCountryID, string ImagePath)
+        {
+
+            int PersonID = -1;
+
+            string query = @"INSERT INTO People (NationalNo, FirstName, SecondName, ThirdName, LastName, DateOfBirth, Gender, Address, Phone, Email)
+                            Values(@NationalNo, @FirstName, @SecondName, @ThirdName, @LastName, @DateOfBirth, @Gender, @Address, @Phone, @Email);
+                            SELECT SCOPE_IDENTITY();";
+
+            using(SqlConnection connection = new SqlConnection(clsDataAccessSettings.connectionString2))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+
+                    command.Parameters.Add("@NationalNo", SqlDbType.NVarChar, 20).Value = NationalNumber;
+                    command.Parameters.Add("@FirstName", SqlDbType.NVarChar, 20).Value = FirstName;
+                    command.Parameters.Add("@SecondName", SqlDbType.NVarChar, 20).Value = SecondName;
+
+                    if (!string.IsNullOrWhiteSpace(ThirdName))
+                    {
+                        command.Parameters.Add("@ThirdName", SqlDbType.NVarChar, 20).Value = ThirdName;
+                    }
+                    else
+                    {
+                        command.Parameters.Add("@ThirdName", SqlDbType.NVarChar).Value = DBNull.Value;
+                    }
+
+                    command.Parameters.Add("@LastName", SqlDbType.NVarChar, 20).Value = LastName;
+                    command.Parameters.Add("@DateOfBirth", SqlDbType.DateTime).Value = DateOfBirth;
+                    command.Parameters.Add("@Gender", SqlDbType.TinyInt).Value = Gender;
+                    command.Parameters.Add("@Address", SqlDbType.NVarChar, 500).Value = Address;
+                    command.Parameters.Add("@Phone", SqlDbType.NVarChar, 20).Value = Phone;
+
+                    if (!string.IsNullOrWhiteSpace(Email))
+                    {
+                        command.Parameters.Add("@Email", SqlDbType.NVarChar, 20).Value = Email;
+                    }
+                    else
+                    {
+                        command.Parameters.Add("@Email", SqlDbType.NVarChar).Value = DBNull.Value;
+                    }
+
+                    command.Parameters.Add("@NationalityCountryID", SqlDbType.Int).Value = NationalityCountryID;
+                    if (!string.IsNullOrWhiteSpace(ImagePath))
+                    {
+                        command.Parameters.Add("@ImagePath", SqlDbType.NVarChar, 250);
+                    }
+                    else
+                    {
+                        command.Parameters.Add("ImagePath", SqlDbType.NVarChar).Value = DBNull.Value;
+                    }
+
+                    try
+                    {
+                        connection.Open();
+
+                        object ID = command.ExecuteScalar();
+
+                        if (ID != null && int.TryParse(ID.ToString(), out int InsertedPersonID))
+                        {
+                            PersonID = InsertedPersonID;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        throw;
+                    }
+                }
+            }
+
+            return PersonID;
         }
     }
 }
